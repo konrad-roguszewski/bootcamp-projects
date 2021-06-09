8ade19721a6b25123d613acbbf49a0aaa882c605
Konrad Roguszewski <aker.hm@gmail.com>
2021-06-09 14:22:26 +0200

    get weather API call

--- project-4/scripts/forecast.js
+++ project-4/scripts/forecast.js
@@ -1,5 +1,19 @@
 const key = '7yA7AXkUrnSkCoHvKq1AlAp6sn3GPzoq';
 
+// get weather information
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+// get city information
 const getCity = async (city) => {
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -12,6 +26,8 @@ const getCity = async (city) => {
 
 };
 
-getCity('warsaw')
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+getCity('warsaw').then(data => {
+    return getWeather(data.Key);
+}).then(data => {
+    console.log(data);
+}).catch(err => console.log(err));
