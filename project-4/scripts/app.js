5dbae765cbc12f83cd2b7a5d8712b494c6ddfd61
Konrad Roguszewski <aker.hm@gmail.com>
2021-06-09 14:40:47 +0200

    updating the location

---
+++ project-4/scripts/app.js
@@ -0,0 +1,27 @@
+const cityForm = document.querySelector('form');
+
+const updateCity = async (city) => {
+
+    const cityDetails = await getCity(city);
+    const weather = await getWeather(cityDetails.Key);
+
+    return {
+        cityDetails: cityDetails,
+        weather: weather
+    };
+
+};
+
+cityForm.addEventListener('submit', event => {
+    // prevent default action
+    event.preventDefault();
+
+    // get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    // update the ui with new city
+    updateCity(city)
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+});
