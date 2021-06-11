499bcb7a0da93fff0ae9420e78114d6cd3e5975d
Konrad Roguszewski <aker.hm@gmail.com>
2021-06-11 11:41:20 +0200

    updating the room and username

--- project-5/scripts/chat.js
+++ project-5/scripts/chat.js
@@ -8,6 +8,7 @@ class Chatroom {
         this.room = room;
         this.username = username;
         this.chats = db.collection('chats');
+        this.unsubscribe;
     }
     async addChat(message){
         // format a chat object
@@ -23,17 +24,27 @@ class Chatroom {
         return response;
     }
     getChats(callback){
-        this.chats
+        this.unsubscribe = this.chats
             .where('room', '==', this.room)
             .orderBy('created_at')
             .onSnapshot(snapshot => {
                 snapshot.docChanges().forEach(change => {
                     if(change.type === 'added'){
                         // update the ui
-                        callback(change.doc.data())
+                        callback(change.doc.data());
                     }
                 });
             });
+    }
+    updateName(username){
+        this.username = username;
+    }
+    updateRoom(room){
+        this.room = room;
+        console.log('room updated');
+        if(this.unsubscribe){
+            this.unsubscribe();
+        }
     }
 }
 
@@ -42,3 +53,12 @@ const chatroom = new Chatroom('general', 'Konrad');
 chatroom.getChats((data) => {
     console.log(data);
 })
+
+setTimeout(() => {
+    chatroom.updateRoom('gaming');
+    chatroom.updateName('Adam');
+    chatroom.getChats((data) => {
+        console.log(data);
+    });
+    chatroom.addChat('hello world');
+}, 3000);
