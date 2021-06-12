5c6d90d5abadb241e7a33e491e03a8ff85032900
Konrad Roguszewski <aker.hm@gmail.com>
2021-06-12 14:07:33 +0200

    tooltips

--- project-6/src/index.js
+++ project-6/src/index.js
@@ -1,3 +1,6 @@
-import './test.css';
+import Tooltip from "./ui/tooltip";
 
-console.log('test')
+// create a tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+
+tooltip.init();
