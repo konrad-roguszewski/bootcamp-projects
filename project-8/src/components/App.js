aa6c21027c3338cfe0a66131da169fb5577cc08a
Konrad Roguszewski <aker.hm@gmail.com>
2021-06-20 13:44:48 +0200

    display setup

--- project-8/src/components/App.js
+++ project-8/src/components/App.js
@@ -1,7 +1,17 @@
-import Signup from "./Signup";
+import Signup from './Signup'
+import { Container } from 'react-bootstrap'
 
 function App() {
-  return <Signup />
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: '100vh' }}
+    >
+      <div className="w-100" style={{ maxWidth: '400px' }}>
+        <Signup />
+      </div>
+    </Container>
+  )
 }
 
-export default App;
+export default App
