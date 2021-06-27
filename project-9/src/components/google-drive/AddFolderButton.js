dd5bc6707c91f5e9d9bb6fb4c248efbb0b4ef23e
Konrad Roguszewski <aker.hm@gmail.com>
2021-06-27 17:08:19 +0200

    adding folders

---
+++ project-9/src/components/google-drive/AddFolderButton.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import { Button, Modal, Form } from 'react-bootstrap'
+import { database } from '../../firebase'
+import { useAuth } from '../../contexts/AuthContext'
+
+export default function AddFolderButton({ currentFolder }) {
+  const [open, setOpen] = useState(false)
+  const [name, setName] = useState("")
+  const { currentUser } = useAuth()
+
+  function openModal() {
+    setOpen(true)
+  }
+
+  function closeModal() {
+    setOpen(false)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    if (currentFolder === null) return
+    // Create folder in database
+
+    database.folders.add({
+      name: name,
+      parentId: currentFolder.id,
+      userId: currentUser.uid,
+      // path,
+      createdAt: database.getCurrentTimestamp(),
+    })
+    setName("")
+    closeModal()
+  }
+
+  return (
+    <>
+      <Button onClick={openModal} variant="outline-success" size="small">
+        Add Folder
+      </Button>
+      <Modal show={open} onHide={closeModal}>
+        <Form onSubmit={handleSubmit}>
+          <Modal.Body>
+            <Form.Group>
+              <Form.Label>Folder Name</Form.Label>
+              <Form.Control
+                type="text"
+                required
+                value={name}
+                onChange={e => setName(e.target.value)}
+              />
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={closeModal}>Close</Button>
+            <Button variant="success" type="submit">Add Folder</Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+    </>
+  )
+}
--- project-9/src/components/google-drive/Dashboard.js
+++ project-9/src/components/google-drive/Dashboard.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { Container } from 'react-bootstrap'
+import AddFolderButton from './AddFolderButton'
 
 export default function Dashboard() {
   return (
     <>
       <Navbar />
       <Container fluid>
-        Content
+        <AddFolderButton />
       </Container>
     </>
   )
