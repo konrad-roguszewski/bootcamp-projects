3c22f18ff7be20555af6b1af39db76ff1c7e39d5
Konrad Roguszewski <aker.hm@gmail.com>
2021-06-27 15:10:56 +0200

    modifying existing code

--- project-9/src/components/authentication/ForgotPassword.js
+++ project-9/src/components/authentication/ForgotPassword.js
@@ -1,7 +1,8 @@
 import React, { useRef, useState } from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap'
-import { useAuth } from '../contexts/AuthContext'
+import { useAuth } from '../../contexts/AuthContext'
 import { Link } from 'react-router-dom'
+import CenteredContainer from './CenteredContainer'
 
 export default function ForgotPassword() {
   const emailRef = useRef()
@@ -26,7 +27,7 @@ export default function ForgotPassword() {
   }
 
   return (
-    <>
+    <CenteredContainer>
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Reset Password</h2>
@@ -49,6 +50,6 @@ export default function ForgotPassword() {
       <div className="w-100 text-center mt-2">
         Need an account? <Link to="/signup">Sign Up</Link>
       </div>
-    </>
+    </CenteredContainer>
   )
 }
--- project-9/src/components/authentication/UpdateProfile.js
+++ project-9/src/components/authentication/UpdateProfile.js
@@ -1,7 +1,8 @@
 import React, { useRef, useState } from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap'
-import { useAuth } from '../contexts/AuthContext'
+import { useAuth } from '../../contexts/AuthContext'
 import { Link, useHistory } from 'react-router-dom'
+import CenteredContainer from './CenteredContainer'
 
 export default function UpdateProfile() {
   const emailRef = useRef()
@@ -35,7 +36,7 @@ export default function UpdateProfile() {
     }
     runUpdates(promises)
       .then(() => {
-        history.push('/')
+        history.push('/user')
       })
       .catch(() => {
         setError('Failed to update account')
@@ -44,7 +45,7 @@ export default function UpdateProfile() {
   }
 
   return (
-    <>
+    <CenteredContainer>
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Update Profile</h2>
@@ -82,8 +83,8 @@ export default function UpdateProfile() {
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-        <Link to="/">Cancel</Link>
+        <Link to="/user">Cancel</Link>
       </div>
-    </>
+    </CenteredContainer>
   )
 }
