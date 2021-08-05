d8e359404665e384c4183b81c93671ddacbcae04
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-05 15:21:41 +0200

    step-4

--- homeworks/slider/slider.js
+++ homeworks/slider/slider.js
@@ -8,11 +8,12 @@ fetch("photos.json")
   })
   .catch(function (error) {
     console.log("error: " + error);
-  });                
-  
-function appendData(data) {
-  const sectionElement = document.getElementById("slider");
+  });
 
+const sectionElement = document.getElementById("slider");
+sectionElement.addEventListener("click", handleClickNextBtn);
+
+function appendData(data) {
   data.forEach(dataElement => {
     const containerElement = document.createElement("div");
     containerElement.classList.add('mySlides');
@@ -21,17 +22,31 @@ function appendData(data) {
     const imageElement = document.createElement("img");
     imageElement.src = dataElement.url;
     containerElement.appendChild(imageElement);
-
+    
     const noteElement = document.createElement("p");
     noteElement.classList.add('text');
     noteElement.innerText = `"${dataElement.caption}" by ${dataElement.author}`;
     containerElement.appendChild(noteElement);
   });
+
+  const nextBtnElement = document.createElement("button");
+  nextBtnElement.innerText = "następne";
+  nextBtnElement.classList.add('next');
+  sectionElement.appendChild(nextBtnElement);
 };
 
 let slideIndex = 1;
 
-function showSlides() {
-  let slides = document.getElementsByClassName("mySlides");
+function showSlides(n) {
+  const slides = document.getElementsByClassName("mySlides");
+  if (n > 2) {slideIndex = 1};
+  [...slides].forEach(slide => slide.style.display = "none");
   slides[slideIndex-1].style.display = "block";
-}
+};
+
+function handleClickNextBtn(event) {
+  if (!event.target.classList.contains('next')){
+    return;
+  };
+  showSlides(slideIndex += 1);
+};
