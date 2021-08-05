1b9f0f8ba44529d8e988651bd0ba5ed45a361f41
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-05 15:19:10 +0200

    step-3

--- homeworks/slider/slider.js
+++ homeworks/slider/slider.js
@@ -4,21 +4,34 @@ fetch("photos.json")
   })
   .then(function (data) {
     appendData(data);
+    showSlides(slideIndex);
   })
   .catch(function (error) {
     console.log("error: " + error);
-  });
-
+  });                
+  
 function appendData(data) {
   const sectionElement = document.getElementById("slider");
-  
+
   data.forEach(dataElement => {
+    const containerElement = document.createElement("div");
+    containerElement.classList.add('mySlides');
+    sectionElement.appendChild(containerElement);
+
     const imageElement = document.createElement("img");
     imageElement.src = dataElement.url;
-    sectionElement.appendChild(imageElement);
+    containerElement.appendChild(imageElement);
 
     const noteElement = document.createElement("p");
-    noteElement.innerText = `Autor: ${dataElement.author}, Opis: ${dataElement.caption}`;
-    sectionElement.appendChild(noteElement);
+    noteElement.classList.add('text');
+    noteElement.innerText = `"${dataElement.caption}" by ${dataElement.author}`;
+    containerElement.appendChild(noteElement);
   });
 };
+
+let slideIndex = 1;
+
+function showSlides() {
+  let slides = document.getElementsByClassName("mySlides");
+  slides[slideIndex-1].style.display = "block";
+}
