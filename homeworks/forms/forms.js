2e4af4d0ac0505cf48dabfde9b5ad94c7a2ec547
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-06 11:33:15 +0200

    step-2

--- homeworks/forms/forms.js
+++ homeworks/forms/forms.js
@@ -2,10 +2,20 @@ const loginForm = document.getElementById('login-form');
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
     signUp();
+    handleAuthStateChanged();
 });
 
 function signUp() {
     const emailValue = document.getElementById('email-input').value;
     const passwordValue = document.getElementById('password-input').value;
     console.log(`e-mail: ${emailValue}, hasło: ${passwordValue}`);
+};
+
+function handleAuthStateChanged() {
+  const emailField = document.getElementById('user-email');
+  const content = document.getElementById('content');
+  const emailValue = document.getElementById('email-input').value;
+  loginForm.style.display = 'none';
+  content.style = {};
+  emailField.textContent = `Użytkownik ${emailValue} został automatycznie zalogowany`;
 };
