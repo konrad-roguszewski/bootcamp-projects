81fef08cb29d55138aaedd6233f4c54d49b81a3e
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-06 11:35:58 +0200

    step-3

--- homeworks/forms/forms.js
+++ homeworks/forms/forms.js
@@ -1,21 +1,51 @@
 const loginForm = document.getElementById('login-form');
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    signUp();
-    handleAuthStateChanged();
+    userRegistration();
+    loginUser();
 });
 
-function signUp() {
-    const emailValue = document.getElementById('email-input').value;
-    const passwordValue = document.getElementById('password-input').value;
-    console.log(`e-mail: ${emailValue}, hasło: ${passwordValue}`);
+loginUser();
+
+function userRegistration() {
+    const userData = {
+        email: document.getElementById('email-input').value,
+        password: document.getElementById('password-input').value,
+    };
+    sessionStorage.setItem('UsersLogin', JSON.stringify(userData));
+    // window.location.reload();
+    console.log(`e-mail: ${userData.email}, hasło: ${userData.password}`);
 };
 
-function handleAuthStateChanged() {
+function loginUser() {
+  const loginEmail = document.getElementById('email-input').value;
+  const loginPass = document.getElementById('password-input').value;
+
   const emailField = document.getElementById('user-email');
   const content = document.getElementById('content');
-  const emailValue = document.getElementById('email-input').value;
-  loginForm.style.display = 'none';
-  content.style = {};
-  emailField.textContent = `Użytkownik ${emailValue} został automatycznie zalogowany`;
+
+  if (sessionStorage.getItem('UsersLogin')) {
+      const loginDeets = JSON.parse(sessionStorage.getItem('UsersLogin'));
+      // console.log(loginDeets);
+      if (loginEmail === loginDeets.email && loginPass === loginDeets.password) {
+          // console.log('Login successful');
+
+          loginForm.style.display = 'none';
+          content.style = {};
+          emailField.textContent = `Użytkownik ${loginDeets.email} został automatycznie zalogowany`;
+
+      } else {
+          // console.log('Wrong credentials');
+
+          loginForm.style.display = 'none';
+          content.style = {};
+          emailField.textContent = `Użytkownik ${loginDeets.email} został automatycznie zalogowany`;
+      }
+  } else {
+      // console.log('Not a registered user');
+
+      content.style.display = 'none';
+      loginForm.style = {};
+      // emailField.textContent = 'No user';
+  };
 };
